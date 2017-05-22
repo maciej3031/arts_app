be54ba7a62d768d734eec50e4f99052f0e7efbb7
maciej3031 <mpesko3031@gmail.com>
2017-05-22 17:37:55 +0200

    virtual-env removed from repository

--- arts_virtual_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\maciej\dropbox\dropbox\praca\programowanie\01_python\02_python_files\02_projects\02_big_projects\10_articles_site\10_articles\arts_virtual_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
