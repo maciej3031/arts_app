a99a35fdcf920d4ba6764e6f9aa2a9dc8301b89a
maciej3031 <mpesko3031@gmail.com>
2017-06-19 22:16:39 +0200

    error (no articles in db) handling added

--- arts/views.py
+++ arts/views.py
@@ -12,33 +12,40 @@ def index(request):
 
 
 def detail(request):
-    article = Article.objects.order_by('?').first()
-    context = {'article': article}
-    return render(request, 'arts/detail.html', context=context)
+    try:
+        article = Article.objects.order_by('?').first()
+        context = {'article': article}
+        return render(request, 'arts/detail.html', context=context)
+    except AttributeError:
+        return HttpResponse(status=404)
 
 
 def advance(request):
-    if request.method == "POST":
-        adv_search = AdvanceSearch(site_id=request.POST['site'],
-                                   category_id=request.POST['category'],
-                                   author=request.POST['author'],
-                                   keywords=request.POST['keywords'],
-                                   date=request.POST['date'],
-                                   rate=request.POST['rate'],)
-        article_query_set = Article.get_article_query_set(adv_search)
-        if not article_query_set:
-            message = messages.error(request, "Nie znaleziono artykułu o podanych kryteriach")
-            context = {'message': message}
-            return render(request, 'arts/index.html', context=context)
+    try:
+        if request.method == "POST":
+            adv_search = AdvanceSearch(site_id=request.POST['site'],
+                                       category_id=request.POST['category'],
+                                       author=request.POST['author'],
+                                       keywords=request.POST['keywords'],
+                                       date=request.POST['date'],
+                                       rate=request.POST['rate'],)
+            article_query_set = Article.get_article_query_set(adv_search)
+            if not article_query_set:
+                message = messages.error(request, "Nie znaleziono artykułu o podanych kryteriach")
+                context = {'message': message}
+                return render(request, 'arts/index.html', context=context)
 
-        article = article_query_set.order_by('?').first()
-        context = {'article': article}
-        return render(request, 'arts/detail.html', context=context)
+            article = article_query_set.order_by('?').first()
+            context = {'article': article}
+            return render(request, 'arts/detail.html', context=context)
 
-    categories = Category.objects.all().order_by('category_name')
-    sites = Site.objects.all().order_by('site_name')
-    context = {'sites': sites, 'categories': categories}
-    return render(request, 'arts/advance.html', context=context)
+        categories = Category.objects.all().order_by('category_name')
+        sites = Site.objects.all().order_by('site_name')
+        context = {'sites': sites, 'categories': categories}
+        return render(request, 'arts/advance.html', context=context)
+
+    except AttributeError:
+        return HttpResponse(status=404)
 
 
 def poll(request):
